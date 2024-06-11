1a4aade5797b1250e2428523e068dd62c23fa053
patacsilpam <pamelampatacsil@gmail.com>
2024-06-11 21:05:55 +0800

    feat: height page

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,7 @@ import { Spotlight } from "./ui/Spotlight"
 
 const Hero = () => {
   return (
-    <div className="pb-20 pt-36">
+    <div className="pb-20 pt-36 h-screen">
         <div>
             <Spotlight
             className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
