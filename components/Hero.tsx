7564d56cb2f44bf47a0a7d594e8f69052c2e9ff2
patacsilpam <pamelampatacsil@gmail.com>
2024-06-12 22:13:01 +0800

    feat: magic button

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,6 @@
 import { Spotlight } from "./ui/Spotlight"
 import { TextGenerateEffect } from "./ui/TextGenerateEffect"
-
+import MagicButton from "./MagicButton";
 const Hero = () => {
   return (
     <div className="pb-20 pt-36">
@@ -56,7 +56,7 @@ const Hero = () => {
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
             Hi! I&apos;m Adrian, a Next.js Developer based in Croatia.
           </p>
-
+          <MagicButton/>
         </div>
       </div>
     </div>
