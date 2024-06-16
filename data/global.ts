3d1c13df1e8fae524b27aac8abcd74ebbf2972b8
patacsilpam <pamelampatacsil@gmail.com>
2024-06-16 22:30:29 +0800

    data

---
+++ data/global.ts
@@ -0,0 +1,6 @@
+export const navItems = [
+    {name:"About", link:"/"},
+    {name:"Projects", link:"#project"},
+    {name:"Testimonials", link:"#testimonials"},
+    {name:"Contact", link:"#contact"}
+]
