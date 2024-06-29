a2ad64ad8a638717dd02112374a137ee4676077f
patacsilpam <pamelampatacsil@gmail.com>
2024-06-29 22:32:28 +0800

    feat: changed name

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -55,7 +55,7 @@ const Hero = () => {
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Adrian, a Next.js Developer based in Croatia.
+            Hi! I&apos;m Pam, a Next.js Developer based in Philippines.
           </p>
           <MagicButton title="Show my work" icon={<FaLocationArrow />} position="right"/>
         </div>
